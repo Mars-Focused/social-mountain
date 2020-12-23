a2ddb3804218c67892b233a18e0471fb616aefdb
Mars_Clarke <marsthecoder@outlook.com>
2020-12-23 16:37:45 -0700

    solved a bug!

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css'
+import "./App.css";
 
-import Header from './Header/Header'
-import Compose from './Compose/Compose'
-import Post from './Post/Post'
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      posts: []
-    }
+      posts: [],
+    };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-    axios.get('https://practiceapi.devmountain.com/api/posts')
-    .then(results => {
-      // console.log(res.data)
-      this.setState({ posts: results.data })
-    })
+    axios
+      .get("https://practiceapi.devmountain.com/api/posts")
+      .then((results) => {
+        // console.log(res.data)
+        this.setState({ posts: results.data });
+      });
   }
 
   updatePost(id, text) {
-    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, {text})
-    .then( results => {
-      this.setState({ posts: results.data })
-    })
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then((results) => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  deletePost() {
+  deletePost() {}
 
-  }
-
-  createPost() {
-
-  }
+  createPost() {}
 
   render() {
     const { posts } = this.state;
@@ -51,19 +49,17 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
           <Compose />
 
-          {
-            posts.map( post => (
-              <Post key={ post.id } 
-              text = {post.text}
-              date = {post.date}
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              text={post.text}
+              date={post.date}
               id={post.id}
               updatePostFn={this.updatePost}
-              />
-            ))
-          }
+            />
+          ))}
         </section>
       </div>
     );
